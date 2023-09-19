07546d939e6fac1105f1e9e108231ee020dfcc2a
Luisdy1972 <luisdy1972@hotmail.com>
2023-09-19 16:17:54 -0500

    page1 maquetada

--- src/page/Registro.jsx
+++ src/page/Registro.jsx
@@ -1,5 +1,5 @@
-import { Input } from '@mui/joy'
 import React from 'react'
+import { Input, Option, Select } from '@mui/joy'
 import { Link } from 'react-router-dom'
 
 // componentes material
@@ -21,10 +21,13 @@ export default function Registro() {
 	// 	setTipoDoc(event.target.value)
 	// }
 
+	function handleChange(event, newValue) {
+		console.log(`Cambiaste a "${newValue}"`)
+	}
+
 	return (
 		// material UI component
 		// <>
-		// 	<h1>reg 1</h1>
 		// 	{/* <TextField
 		// 		sx={{ width: '17rem', borderRadius: '50%' }}
 		// 		size="small"
@@ -88,7 +91,7 @@ export default function Registro() {
 		// 	<TextField
 		// 		type="date"
 		// 		className="input-text"
-		// 		id="fehca nacimientto"
+		// 		id="fehca nacimiento"
 		// 		variant="filled"
 		// 		required
 		// 	/>
@@ -110,17 +113,64 @@ export default function Registro() {
 		// Joy UI
 		<>
 			<Input
-				placeholder="Nombre"
-				sx={{ borderRadius: '10rem' }}
+				placeholder="Nombre*"
+				sx={{ borderRadius: '15px', minWidth: '16rem' }}
 				variant="soft"
 				required
 			/>
 			<Input
-				placeholder="Apellido"
-				sx={{ borderRadius: '10rem' }}
+				placeholder="Apellido*"
+				sx={{ borderRadius: '15px', minWidth: '16rem' }}
 				variant="soft"
 				required
 			/>
+			<Select
+				sx={{ borderRadius: '15px', minWidth: '16rem' }}
+				variant="soft"
+				required
+				defaultValue=""
+				onChange={handleChange}
+			>
+				<Option value="">Tipo de documento*</Option>
+				<Option value="Ti">Ti</Option>
+				<Option value="Cc">Cc</Option>
+				<Option value="Pa">Pa</Option>
+			</Select>
+			<Input
+				placeholder="Número de documento*"
+				sx={{ borderRadius: '15px', minWidth: '16rem' }}
+				variant="soft"
+				required
+			/>
+			<Input
+				placeholder="Correo institucional*"
+				sx={{ borderRadius: '15px', minWidth: '16rem' }}
+				variant="soft"
+				required
+			/>
+			<Input
+				placeholder="Correo Personal"
+				sx={{ borderRadius: '15px', minWidth: '16rem' }}
+				variant="soft"
+				required
+			/>
+			<div style={{ display: 'block' }}>
+				<div style={{ textAlign: 'center' }}>Fecha de nacimiento</div>
+				<Input
+					type="date"
+					sx={{ borderRadius: '15px' }}
+					variant="soft"
+					required
+				/>
+			</div>
+			<div className="navegacion ">
+				<Link to="/">
+					<div className="button-navegacion">{'<<Atrás'}</div>
+				</Link>
+				<Link to="/registro/2">
+					<div className="button-navegacion">{'Siguiente>>'}</div>
+				</Link>
+			</div>
 		</>
 	)
 }
